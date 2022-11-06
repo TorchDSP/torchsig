b7d7975fd751716b0511768544211de57e72d252
Luke Boegner <luke.boegner@gmail.com>
2022-11-06 15:29:52 +0000

    add missing req and update dataset init

--- torchsig/datasets/__init__.py
+++ torchsig/datasets/__init__.py
@@ -2,3 +2,6 @@ from .synthetic import *
 from .modulations import *
 from .sig53 import Sig53
 from .radioml import *
+from .wideband import *
+from .wideband_sig53 import WidebandSig53
+from .file_datasets import *
