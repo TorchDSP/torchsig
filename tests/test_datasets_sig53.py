5a96b2ac18a2068a8e795680b0ca2dfdec1ff93a
Garrett Vanhoy <gvanhoy@peratonlabs.com>
2023-03-31 15:08:45 -0400

    Instrumented sig53 for testing. Not ideal, but it's a start.

--- tests/test_datasets_sig53.py
+++ tests/test_datasets_sig53.py
@@ -1,8 +1,13 @@
 from torchsig.datasets.sig53 import Sig53
 from unittest import TestCase
+import os
 
 
 class GenerateSig53(TestCase):
     def test_can_generate_sig53_clean_train(self):
-        x = 2 + 2
-        self.assertEqual(x, 4)
+        ds = Sig53(root=".", regenerate=True, generation_test=True)
+        first_data = ds[0][0]
+        ds = Sig53(root=".", regenerate=True, generation_test=True)
+        second_data = ds[0][0]
+
+        self.assertEqual(first_data.all(), second_data.all())
