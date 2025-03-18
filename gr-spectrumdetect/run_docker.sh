75799d87b13aaf7642418a095023899d824324e6
Matt Carrick <matthew.carrick@peratonlabs.com>
2025-03-18 14:43:04 -0400

    enabling USB-based device permissions

--- gr-spectrumdetect/run_docker.sh
+++ gr-spectrumdetect/run_docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -it --rm --gpus all --network=host -e DISPLAY=$DISPLAY -v $HOME/.Xauthority:/root/.Xauthority -v /:/workspace/ --name grspectrumdetect -t grspectrumdetect:v01 /bin/bash
+docker run --privileged -v /etc/udev/rules.d/:/etc/udev/rules.d/ -it --rm --gpus all --network=host -e DISPLAY=$DISPLAY -v $HOME/.Xauthority:/root/.Xauthority -v /:/workspace/ --name grspectrumdetect -t grspectrumdetect:v01 /bin/bash
+
