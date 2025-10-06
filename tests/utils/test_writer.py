5f541d66ce1daff7555b0c50c884dd435cfc6dcc
Ere Oh <eoh@ltsnet.net>
2025-10-06 17:43:05 +0000

    Closes #432

--- tests/utils/test_writer.py
+++ tests/utils/test_writer.py
@@ -23,6 +23,7 @@ import pytest
 from typing import List, Any, Tuple
 from collections.abc import Iterable
 import itertools
+from time import time
 
 
 RTOL = 1E-6
@@ -185,31 +186,78 @@ def test_DatasetCreator_targets(
     assert dc.overwrite
     assert isinstance(dc.get_writing_info_dict(), dict)
 
+def test_DatasetCreator_tqdm():
+    seed = 1234567890
+    dataset_length = 500
+    batch_size = 2
+
+    md = DatasetMetadata(
+        num_iq_samples_dataset = 4096,
+        fft_size = 64,
+        sample_rate = 10e6,
+        num_signals_max = 1,
+        num_signals_min = 1,
+    )
+
+    ds = TorchSigIterableDataset(
+        dataset_metadata = md,
+        seed = seed,
+        transforms=[],
+        target_labels=None
+    )
+    dl = WorkerSeedingDataLoader(
+        ds, 
+        batch_size = batch_size, 
+        collate_fn=lambda x: x
+    )
+    
+    # save dataset to disk
+    print("Single threaded")
+    dc = DatasetCreator(
+        dataloader=dl,
+        dataset_length = dataset_length,
+        root = wb_data_dir,
+        overwrite = True,
+        multithreading=False   
+    )
+    dc.create()
+
+    print("Multithreading")
+    dc2 = DatasetCreator(
+        dataloader=dl,
+        dataset_length = dataset_length,
+        root = wb_data_dir,
+        overwrite = True,
+        multithreading=True   
+    )
+    dc2.create()
+
 
 if __name__ == "__main__":
+    test_DatasetCreator_tqdm()
     # test_DatasetCreator(params={'dataset_length': 10}, is_error=False)
-    transforms = [[YOLOLabel()]]
-    target_labels = [
-        ["class_name"], # ofdm-300
-        ["class_index"], # 41
-        ["class_name", "class_index"], # ('ofdm-300', 41)
-        ["yolo_label"], # (41, 0.2279052734375, 0.25370860860317335, 0.199951171875, 0.078125)
-        ["class_name", "yolo_label"] #('ofdm-300', (41, 0.2279052734375, 0.25370860860317335, 0.199951171875, 0.078125))
-    ]
-    num_signals_min = [0]
-    num_signals_max = [1, 3]
-    params = itertools.product(
-        transforms,
-        target_labels,
-        num_signals_min,
-        num_signals_max
-    )
-    # breakpoint()
-    for p in params:
-        print(f"{p} -------")
-        try:
-            test_DatasetCreator_targets(*p)
-        except ImportError:
-            pass
+    # transforms = [[YOLOLabel()]]
+    # target_labels = [
+    #     ["class_name"], # ofdm-300
+    #     ["class_index"], # 41
+    #     ["class_name", "class_index"], # ('ofdm-300', 41)
+    #     ["yolo_label"], # (41, 0.2279052734375, 0.25370860860317335, 0.199951171875, 0.078125)
+    #     ["class_name", "yolo_label"] #('ofdm-300', (41, 0.2279052734375, 0.25370860860317335, 0.199951171875, 0.078125))
+    # ]
+    # num_signals_min = [0]
+    # num_signals_max = [1, 3]
+    # params = itertools.product(
+    #     transforms,
+    #     target_labels,
+    #     num_signals_min,
+    #     num_signals_max
+    # )
+    # # breakpoint()
+    # for p in params:
+    #     print(f"{p} -------")
+    #     try:
+    #         test_DatasetCreator_targets(*p)
+    #     except ImportError:
+    #         pass
         # breakpoint()
     # test_padding()
