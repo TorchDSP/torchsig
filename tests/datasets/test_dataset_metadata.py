c6f471e2a21c2d31258f221ac20d0292c4d19dcf
Ere Oh <eoh@ltsnet.net>
2025-10-07 17:21:05 +0000

    dataset metadata coverage complete

--- tests/datasets/test_dataset_metadata.py
+++ tests/datasets/test_dataset_metadata.py
@@ -40,3 +40,36 @@ def test_DatasetMetadata():
     )
     md.to_dict()
 
+    assert isinstance(str(md), str)
+    assert isinstance(repr(md), str)
+    
+    assert isinstance(md.dataset_center_freq_max, float)
+    assert isinstance(md.dataset_center_freq_min, float)
+    assert md.dataset_center_freq_min < md.dataset_center_freq_max
+
+    assert isinstance(md.dataset_duration_max, float)
+    assert isinstance(md.dataset_duration_min, float)
+    assert md.dataset_duration_min < md.dataset_duration_max
+
+    assert isinstance(md.dataset_duration_in_samples_max, int)
+    assert isinstance(md.dataset_duration_in_samples_min, int)
+    assert md.dataset_duration_in_samples_min < md.dataset_duration_in_samples_max
+
+    assert isinstance(md.dataset_bandwidth_max, float)
+    assert isinstance(md.dataset_bandwidth_min, float)
+    assert md.dataset_bandwidth_min < md.dataset_bandwidth_max
+
+    assert isinstance(md.signal_duration_in_samples_max, int)
+    assert isinstance(md.signal_duration_in_samples_min, int)
+    assert md.signal_duration_in_samples_min < md.signal_duration_in_samples_max
+
+    assert isinstance(md.fft_frequency_resolution, float)
+    
+    assert isinstance(md.fft_frequency_max, float)
+    assert isinstance(md.fft_frequency_min, float)
+    assert md.fft_frequency_min < md.fft_frequency_max
+
+    assert isinstance(md.frequency_max, float)
+    assert isinstance(md.frequency_min, float)
+    assert md.frequency_min < md.frequency_max
+
